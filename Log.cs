cb03e25b4fa59769a5d3f0b5e69df8e7cbd10d29
Luiz Avila <lapavila@gmail.com>
2012-11-18 21:41:49 -0200

    fixing initial config

---
+++ Log.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using log4net;
+
+namespace LogLibrary
+{
+    public class Log
+    {
+        private ILog _log;
+
+        public Log(ILog _log)
+        {
+            this._log = _log;
+        }
+
+        public void Fatal(string message)
+        {
+            if (IsFatalEnabled)
+                _log.Fatal(message);
+        }
+
+        public void Error(string message)
+        {
+            if (IsErrorEnabled)
+                _log.Error(message);
+        }
+
+        public void Warn(string message)
+        {
+            if (IsWarnEnabled)
+                _log.Warn(message);
+        }
+
+        public void Info(string message)
+        {
+            if (IsInfoEnabled)
+                _log.Info(message);
+        }
+
+        public void Debug(string message)
+        {
+            if (IsDebugEnabled)
+                _log.Debug(message);
+        }
+
+        public bool IsDebugEnabled
+        {
+            get { return _log.IsDebugEnabled; }
+        }
+
+        public bool IsErrorEnabled
+        {
+            get { return _log.IsErrorEnabled; }
+        }
+
+        public bool IsFatalEnabled
+        {
+            get { return _log.IsErrorEnabled; }
+        }
+
+        public bool IsInfoEnabled
+        {
+            get { return _log.IsInfoEnabled; }
+        }
+
+        public bool IsWarnEnabled
+        {
+            get { return _log.IsWarnEnabled; }
+        }
+
+        public log4net.Core.ILogger Logger
+        {
+            get { return _log.Logger; }
+        }
+    }
+}
--- Logger.cs
+++ Logger.cs
@@ -1,144 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using log4net;
-using System.Configuration;
-using log4net.Core;
-using log4net.Config;
 using System.IO;
+using log4net.Config;
+using log4net;
+using System.Reflection;
+using System.Configuration;
 
 namespace LogLibrary
 {
     public class Logger
     {
-        public enum LogType
-        {
-            None = 0,
-            File = 1,
-            Email = 2,
-            ClientLog = 3,
-            Sql = 4
-        }
-        public enum LogLevel
-        {
-            Debug = 1,
-            Error = 2,
-            Fatal = 3,
-            Info = 4,
-            Warn = 5
-        }
-        private readonly ILog log;
-        private LogType logtype;
+        private static Log _log;
 
-        public Logger(Type type)
+        public static Log GetLogger(Type type)
         {
-            logtype = LogType.None;
+            if (LogManager.GetCurrentLoggers().Length == 0)
+            {
+                LoadConfig();
+                _log = new Log(LogManager.GetLogger(type));
+            }
+            return _log;
+        }
 
+        private static void LoadConfig()
+        {
+            var log4NetConfigFilePath = LoadCustomtConfig();
+            if (log4NetConfigFilePath == null)
+            {
+                log4NetConfigFilePath = LoadDefaultConfig();
+            }
+
+            Configure(log4NetConfigFilePath);
+        }
+
+        private static string LoadDefaultConfig()
+        {
             var log4NetConfigDirectory = AppDomain.CurrentDomain.RelativeSearchPath ?? AppDomain.CurrentDomain.BaseDirectory;
             var log4NetConfigFilePath = Path.Combine(log4NetConfigDirectory, "Assets\\log4net.config");
-            
-            XmlConfigurator.ConfigureAndWatch(new FileInfo(log4NetConfigFilePath));
-
-            log = LogManager.GetLogger(type);
+            return log4NetConfigFilePath;
         }
 
-        public Logger(Type type, LogType logtype) : this(type)
+        private static string LoadCustomtConfig()
         {
-            this.logtype = logtype;
-        }
+            string configFilePath = ConfigurationManager.AppSettings["log.config"];
+            string log4NetConfigFilePath = null;
 
-        public void Log(LogLevel level, object message, Exception exception, LogType logtype)
-        {
-            switch (level)
+            if (configFilePath != null)
             {
-                case LogLevel.Debug:
-                    this.Debug(message, exception, logtype);
-                    break;
-                case LogLevel.Error:
-                    this.Error(message, exception, logtype);
-                    break;
-                case LogLevel.Fatal:
-                    this.Fatal(message, exception, logtype);
-                    break;
-                case LogLevel.Info:
-                    this.Info(message, exception, logtype);
-                    break;
-                case LogLevel.Warn:
-                    this.Warn(message, exception, logtype);
-                    break;
+                var log4NetConfigDirectory = AppDomain.CurrentDomain.RelativeSearchPath ?? AppDomain.CurrentDomain.BaseDirectory;
+                log4NetConfigFilePath = Path.Combine(log4NetConfigDirectory, configFilePath);
             }
-        }
-        public void Log(LogLevel level, object message)
-        {
-            this.Log(level, message, null, this.logtype);
-        }
-        public void Log(LogLevel level, object message, Exception exception)
-        {
-            this.Log(level, message, exception, this.logtype);
-        }
-        public void Log(LogLevel level, object message, LogType logtype)
-        {
-            this.Log(level, message, null, logtype);
+            return log4NetConfigFilePath;
         }
 
-        private void Debug(object message, Exception exception)
+        private static void Configure(string log4NetConfigFilePath)
         {
-            log.Debug(message, exception);
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(log4NetConfigFilePath));
         }
 
-        private void Debug(object message, Exception exception, LogType logtype)
-        {
-            logtype = this.logtype == LogType.None ? logtype : this.logtype;
-            log4net.ThreadContext.Properties["LogType"] = logtype.ToString().ToLowerInvariant();
-            log.Debug(message, exception);
-        }
-
-        private void Error(object message, Exception exception)
-        {
-            log.Error(message, exception);
-        }
-
-        private void Error(object message, Exception exception, LogType logtype)
-        {
-            logtype = this.logtype == LogType.None ? logtype : this.logtype;
-            log4net.ThreadContext.Properties["LogType"] = logtype.ToString().ToLowerInvariant();
-            log.Error(message, exception);
-        }
-
-
-        private void Fatal(object message, Exception exception)
-        {
-            log.Fatal(message, exception);
-        }
-        private void Fatal(object message, Exception exception, LogType logtype)
-        {
-            logtype = this.logtype == LogType.None ? logtype : this.logtype;
-            log4net.ThreadContext.Properties["LogType"] = logtype.ToString().ToLowerInvariant();
-            log.Fatal(message, exception);
-        }
-
-        private void Info(object message, Exception exception)
-        {
-            log.Info(message, exception);
-        }
-        private void Info(object message, Exception exception, LogType logtype)
-        {
-            logtype = this.logtype == LogType.None ? logtype : this.logtype;
-            log4net.ThreadContext.Properties["LogType"] = logtype.ToString().ToLowerInvariant();
-            log.Info(message, exception);
-        }
-
-        private void Warn(object message, Exception exception)
-        {
-            log.Warn(message, exception);
-        }
-
-        private void Warn(object message, Exception exception, LogType logtype)
-        {
-            logtype = this.logtype == LogType.None ? logtype : this.logtype;
-            log4net.ThreadContext.Properties["LogType"] = logtype.ToString().ToLowerInvariant();
-            log.Warn(message, exception);
-        }
     }
 }
